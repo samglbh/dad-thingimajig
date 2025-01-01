b7f50f85011f185029879db018003f26386d89b3
samglbh <155912005+samglbh@users.noreply.github.com>
2025-01-01 20:09:01 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,34 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.SmallDiamond)
+    turn()
+    basic.showIcon(IconNames.Diamond)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+function turn () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    basic.pause(200)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+}
+pins.digitalWritePin(DigitalPin.P0, 0)
+basic.showIcon(IconNames.SmallDiamond)
+basic.pause(500)
+basic.showIcon(IconNames.Diamond)
 let rand = randint(120000, 300000)
 loops.everyInterval(rand, function () {
-    pins.digitalWritePin(DigitalPin.P0, 1)
-    basic.pause(5000)
-    pins.digitalWritePin(DigitalPin.P0, 0)
+    turn()
     rand = randint(120000, 300000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
 })
